42e36185c72c4d80d3027057d0788ebed63f00a9
chesfiy <chesify@gmail.com>
2025-09-24 18:59:34 +0300

    vapi intergation

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,20 +1,28 @@
 "use client"
-import { useMutation, useQuery } from "convex/react"
-import { api } from "@workspace/backend/_generated/api";
-import { Input } from "@workspace/ui/components/input"
+import { useVapi } from "@/modules/widget/hooks/use-vapi";
 import { Button } from "@workspace/ui/components/button";
 
 
 export default function Page() {
-  const users = useQuery(api.users.getMany);
-  const adduser = useMutation(api.users.add); 
+  
+  const {
+      isConnected, 
+      isConnecting, 
+      isSpeaking, 
+      transcript, 
+      startCall, 
+      endCall,
+    } = useVapi();
   return (
-    <div className="flex flex-col  items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">App/Widget</h1>
-        <Button onClick={() => adduser()}>Add User</Button>
-        {JSON.stringify(users)}
-      </div>
+
+    <div className="flex flex-col  items-center justify-center min-h-svh max-w-md mx-auto w-full">
+        <Button onClick={() => startCall()}>Start Call</Button>
+        <Button onClick={() => endCall()} variant={"destructive"}>End Call</Button>
+
+        <p>Isconnected: {`${isConnected}`}</p>
+        <p>Isconnecting: {`${isConnecting}`}</p>
+        <p>Isspeaking: {`${isSpeaking}`}</p>
+        <p>Transcript: {`${JSON.stringify(transcript,null,2)}`}</p>
     </div>
   )
 }
